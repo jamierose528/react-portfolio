bbbf289d8655fcdb939bfd425f130387d11d9319
Paper Girl <jamie.rose.528@gmail.com>
2022-02-22 16:51:34 -0800

    added navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,2 +1,17 @@
 import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
+<Navbar bg="light" variant="light" sticky="top">
+  <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about-me">About Me</Nav.Link>
+      <Nav.Link href="#wiboo">Wiboo</Nav.Link>
+      <Nav.Link href="#passport-quest">Passport Quest</Nav.Link>
+      <Nav.Link href="#red-orchid">Red Orchid</Nav.Link>
+    </Nav>
+  </Container>
+</Navbar>;
+
+export default Navbar;
